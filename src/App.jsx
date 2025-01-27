c4c99a036022d2f6b24ee6e578d21cd8a45c6952
roufkhalid93 <roufkhalid93@gmail.com>
2025-01-27 23:34:55 +0800

    add routes & loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { 
+  createBrowserRouter, 
+  RouterProvider 
+} from "react-router-dom"
+
+//routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard"
+import Error from "./pages/Error"
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader
+  },
+  {
+    path: "*",
+    element: <Error />
+  }
+])
 
 
 function App() {
@@ -5,7 +25,7 @@ function App() {
   return (
     <>
       <div className="App">
-        Test
+        <RouterProvider router={router} />
       </div>
     </>
   )
