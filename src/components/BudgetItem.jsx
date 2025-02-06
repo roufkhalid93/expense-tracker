89663f0953bf68a4a20f1a789e751efe21d2ee6c
roufkhalid93 <roufkhalid93@gmail.com>
2025-02-06 17:04:37 +0800

    add budget card feature

---
+++ src/components/BudgetItem.jsx
@@ -0,0 +1,31 @@
+//helpers
+import { calculatSpentByBudget, formatCurrency, formatPercentage } from "../helpers";
+
+
+const BudgetItem = ({budget}) => {
+    const {id, name, amount, color}= budget;
+    const spent = calculatSpentByBudget (id)
+
+    return (
+        <div 
+            className="budget"
+            style={{
+                "--accent": color
+            }}
+        >
+            <div className="progress-text">
+                <h3>{name}</h3>
+                <p>{formatCurrency(amount)} Budgeted</p>
+            </div>
+            <progress max={amount} value={spent}>
+                {formatPercentage(spent/amount)}
+            </progress>
+            <div className="progress-text">
+                <small>{formatCurrency(spent)} spent</small>
+                <small>{formatCurrency(amount - spent)} remaining</small>
+            </div>
+        </div>
+    )
+}
+
+export default BudgetItem
--- src/helpers.js
+++ src/helpers.js
@@ -42,3 +42,32 @@ export const createExpense = ({name, amount, budgetId}) => {
     const existingExpenses= fetchData("expenses") ?? []
     return localStorage.setItem("expenses", JSON.stringify([...existingExpenses, newItem]))
 }
+
+//format currency
+export const formatCurrency = (amt) => {
+    return amt. toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+}
+
+//format percentages
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "percent", 
+        minimumFractionDigits: 0,
+    })
+}
+
+//total spent by budget
+export const calculatSpentByBudget = (budgetId) => {
+    const expenses = fetchData("expenses") ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        //check if expense.id === budgetId I passed in
+        if(expense.budgetId !== budgetId) return acc
+
+        //add the current amount to my total
+        return acc += expense.amount
+    }, 0)
+    return budgetSpent;
+}
