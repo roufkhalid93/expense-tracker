61d145714c2fd891088438885dcd61f6fe70ddca
roufkhalid93 <roufkhalid93@gmail.com>
2025-02-07 08:18:18 +0800

    add budget link and delete item feature

--- src/helpers.js
+++ src/helpers.js
@@ -12,11 +12,6 @@ export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key))
 }
 
-//delete item
-export const deleteItem = ({key}) => {
-    return localStorage.removeItem(key)
-}
-
 //create budget
 export const createBudget = ({name, amount}) => {
     const newItem = {
@@ -55,6 +50,22 @@ export const calculatSpentByBudget = (budgetId) => {
         return acc += expense.amount
     }, 0)
     return budgetSpent;
+}
+
+//get all item from local storage
+export const getAllMatchingItems = ({category, key, value}) => {
+    const data = fetchData(category) ?? [];
+    return data.filter((item) => item[key] === value);
+}
+
+//delete item from local storage
+export const deleteItem = ({ key, id}) => {
+    const existingData = fetchData(key);
+    if(id){
+        const newData = existingData.filter((item) => item.id !== id);
+        return localStorage.setItem(key, JSON.stringify(newData));
+    }
+    return localStorage.removeItem(key);
 }
 
 
